62840320dcfef168fddecee82959295f8ec379fe
Brit Broetje <brittanybroetje@gmail.com>
2016-09-01 17:43:01 -0700

    js to add card divs in loop

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,17 +1,33 @@
 console.log("JS file is connected to HTML! Woo!")
 
+
 var cardOne = "queen";
 var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
 
+var gameBoard = document.getElementById('game-board');
 
-if (((cardTwo === "queen") && (cardFour === "queen")) || 
+var createBoard = function() {
+		var newCard = document.createElement('div');
+		newCard.className = 'card';
+		gameBoard.appendChild(newCard);
+	};
+
+for (x = 0; x < 4; x += 1) {
+	createBoard(x);
+}
+
+
+
+
+
+/*if (((cardTwo === "queen") && (cardFour === "queen")) || 
 	((cardTwo === "king") && (cardFour === "king"))) {
 	alert("You found a match!");
 } else {
 	alert("Sorry, try again.");
-};
+};*/
 
 
 
