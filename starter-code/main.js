2dc7f916518d45e87e71719a93d26249ed12f1ab
Brit Broetje <brittanybroetje@gmail.com>
2016-09-08 12:22:18 -0700

    memory game up to flipping cards

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,5 +1,8 @@
 console.log("JS file is connected to HTML! Woo!")
 
+var cards = ['queen', 'queen', 'king', 'king'];
+var cardsInPlay = [];
+
 
 var cardOne = "queen";
 var cardTwo = "queen";
@@ -8,32 +11,55 @@ var cardFour = "king";
 
 var gameBoard = document.getElementById('game-board');
 
+function flipCard() {
+	if (this.getAttribute('data-card') == 'king') {
+		this.className = "kingbg";
+	}
+	else if (this.getAttribute('data-card') == 'queen') {
+		this.className = "queenbg";
+	}
+};
+
+
+function isMatch() {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match!");
+	} else {
+		alert("Sorry, try again.");
+	}
+};
+
+
+function isTwoCards() {
+	cardsInPlay.push(this.getAttribute('data-card'));
+	if (cardsInPlay.length === 2) {
+		isMatch(cardsInPlay);
+		cardsInPlay = [];
+	};
+};
+
+
+
+
 var createBoard = function() {
+	for (i = 0; i < cards.length; i++) {
 		var newCard = document.createElement('div');
 		newCard.className = 'card';
+		if (i > 2) {
+			newCard.style.float = "left";
+		};
 		gameBoard.appendChild(newCard);
-	};
-
-for (x = 0; x < 4; x += 1) {
-	createBoard(x);
-}
-
-
+		newCard.addEventListener('click', flipCard);
+		newCard.setAttribute('data-card', cards[i]);
+		newCard.addEventListener('click', isTwoCards);
+	}
+};
 
 
-
-/*if (((cardTwo === "queen") && (cardFour === "queen")) || 
-	((cardTwo === "king") && (cardFour === "king"))) {
-	alert("You found a match!");
-} else {
-	alert("Sorry, try again.");
-};*/
+createBoard();
 
 
 
-/*
-if (cardOne === cardTwo) {
-	alert("You found a match!");
-} else {
-	alert("Sorry, try again.");
-};*/
+
+
+
